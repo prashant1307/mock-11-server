5c1b6369d338ea1ff5e9c8bbb83ac6e713442604
prashant1307 <prashantlorgev8@gmail.com>
2023-01-20 17:02:03 +0530

    C:/Program Files/Git/

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get("/",(req,res)=>{
+    res.send("welcome to server")
+})
 
 //Routes
 app.post("/signup", async(req, res) => {
